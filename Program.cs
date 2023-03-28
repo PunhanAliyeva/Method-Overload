01ac10d74046ead6e8f7117181077ce94ef1dca8
PunhanAliyeva <eliyevapunhan567@gmail.com>
2023-03-28 20:20:36 +0400

    first task

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 23, 34, 12, 7,45, 67, 56, 78 };
+            Console.WriteLine(Min(num));
+        }
+            public static int Min(int[] num)
+            {
+                int min = num[0];
+                for (int i = 0; i < num.Length; i++)
+                    if (min > num[i])
+                    {
+                        min = num[i];
+                    }
+                return min;
+
+            }
+        }
+    }
---
+++ Program1.cs
@@ -0,0 +1,31 @@
+﻿namespace Overload_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area(3,1.2f);
+            Area(6,7);
+            Area(2, 5, 8);
+            Area(10,8, 6, 4, 10);
+        }
+        public static void Area(int p,float r)
+        {
+            Console.WriteLine(p* r * r);
+        }
+        public static void Area(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+        public static void Area(int a, int b ,int c)
+        {
+            Console.WriteLine(2*(a*b+a*c+b*c));
+        }
+        public static async void Area(int p,int r,int a,int b,int c)
+        {
+            p = (a + b + c) / 2;
+            Console.WriteLine( p*r);
+        }
+
+    }
+}
